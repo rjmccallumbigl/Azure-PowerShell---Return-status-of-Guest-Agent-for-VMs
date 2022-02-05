aa83753ef7c2e81f3f5ecd9bf379f704b95e6628
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-02-05 12:55:15 -0500

    Add authentication

--- returnGAStatuses.ps1
+++ returnGAStatuses.ps1
@@ -1,5 +1,28 @@
+<##########################################################################################################
+
+	.SYNOPSIS
+		Return verbose status of Guest Agent for Azure VMs.
+ 
+##########################################################################################################>
+# https://github.com/microsoft/MSLab/tree/master/Scenarios/Running%20WSLab%20in%20Azure#creating-vm-with-powershell
+# Set-execution policy to remote signed for current process
+if ((Get-ExecutionPolicy) -ne "RemoteSigned"){Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force}
+
+# Download Azure module
+if (!(Import-Module -Name Az -ErrorAction Ignore)){
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module -Name AZ -Force
+}
+
+Login-AzAccount -UseDeviceAuthentication
+
+#select context if more available
+$context=Get-AzContext -ListAvailable
+if (($context).count -gt 1){
+    $context | Out-GridView -OutputMode Single | Set-AzContext
+}
+
 # Get all VMs
-Connect-AzAccount
 $vms = Get-AzVM
 
 # For each returned VM, display Resource URI and Guest Agent status
