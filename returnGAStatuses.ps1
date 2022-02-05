63575bed7f17f37acb0c293f3af585d3b4580b0a
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-02-05 12:44:15 -0500

    Change Write-Host to Write-Output

--- returnGAStatuses.ps1
+++ returnGAStatuses.ps1
@@ -4,12 +4,12 @@ $vms = Get-AzVM
 
 # For each returned VM, display Resource URI and Guest Agent status
 foreach ($vm in $vms) {
-	Write-Host $vm.Id
+	Write-Output $vm.Id
 	$vmStatus = Get-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Status	
 	foreach ($status in $vmStatus.Statuses) {
 		if ($status.DisplayStatus -like "*deallocat*") {
 			# 
-			Write-Host "VM is deallocated, Start VM to successfully grab state of Guest Agent"
+			Write-Output "VM is deallocated, Start VM to successfully grab state of Guest Agent"
 			break
 		}	
 	}
